29e232973ead683d7950a7c44d9a3afa87c0dd36
Khurshid Khan <khurshidsunny45@gmail.com>
2024-06-29 05:23:12 +0500

    created a simple schema and model

--- server.js
+++ server.js
@@ -25,6 +25,27 @@ mongoose
     console.log('DB connection successfull!');
   });
 
+// schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// model
+const Tour = mongoose.model('Tour', tourSchema);
+
 // const port = process.env.PORT || 3000;
 const port = 3000;
 app.listen(port, () => {
