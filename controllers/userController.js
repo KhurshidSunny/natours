25417762c926c5236cb3b018c59aef847572e723
Khurshid Khan <khurshidsunny45@gmail.com>
2024-07-13 12:04:55 +0500

    Refactor: Added factory handlers for the controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -11,31 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
   return newObj;
-};
-
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-
-  res.status(400).json({
-    status: 'success',
-    data: {
-      users,
-    },
-  });
-});
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The is route is not implemented yet',
-  });
-};
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The is route is not implemented yet',
-  });
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
@@ -65,12 +41,25 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateUser = (req, res) => {
+// middleware for get own information
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+
+exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'The is route is not implemented yet',
+    message: 'The is route is not defined. Please used signup instead',
   });
 };
+
+// DO NOT UPDATE PASSWORD WITH THIS
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
@@ -80,10 +69,3 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'The is route is not implemented yet',
-  });
-};
