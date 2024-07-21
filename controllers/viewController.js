787032c55efae41fbbf73f3bfab70316f709224a
Khurshid Khan <khurshidsunny45@gmail.com>
2024-07-21 12:49:46 +0500

    Feature (view): Dynamically render the Overview page and Tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,12 +1,32 @@
 /* eslint-disable prettier/prettier */
-exports.getOverview = (req, res, next) => {
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res, next) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // 2) Build the template
+
+  // 3) Rendering the data  using data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
+    tour,
   });
-};
+});
