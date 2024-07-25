9c81a84c1fdc3640202f1a3510a5561c69228830
Khurshid Khan <khurshidsunny45@gmail.com>
2024-07-25 11:11:29 +0500

    Feature: upload image

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use('/api', rateLimiter);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
-  console.log(req.cookies);
+  // console.log(req.cookies);
 
   next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,10 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
 
-  photo: String,
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
